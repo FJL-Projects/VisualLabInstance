7b7d59c665fbd8d0a7e4472926e0f1d47937efbe
0x11111111 <dypro_mike@outlook.com>
2024-03-20 14:25:33 +0800

    Waiting for smoothing modified mesh.

--- src/algorithm/meshTransform.h
+++ src/algorithm/meshTransform.h
@@ -69,3 +69,32 @@ int SurfaceMeshToPolyData(SurfaceMesh& mesh, vtkSmartPointer<vtkPolyData>& polyD
 	return 0;
 
 }
+
+int PolyDataToSurfaceMesh(vtkPolyData* polyData, SurfaceMesh& surfaceMesh)
+{
+	vtkPoints* points = polyData->GetPoints();
+	int numberOfPoints = polyData->GetNumberOfPoints();
+
+	if (0 == numberOfPoints || points == NULL)
+		return -1;
+
+	for (int i = 0; i < numberOfPoints; i++)
+	{
+		double point[3];
+		points->GetPoint(i, point);
+		surfaceMesh.add_vertex(Kernel::Point_3(point[0], point[1], point[2]));
+	}
+
+	vtkCellArray* triangles = polyData->GetPolys();
+	triangles->InitTraversal();
+
+	//vtkIdType npts = 3, * cell;
+	vtkIdType npts;               // 存储单元的点数
+	vtkIdType* cell;         // 存储单元的点索引数组
+	while (triangles->GetNextCell(npts, cell))
+	{
+		surfaceMesh.add_face(CGAL::SM_Vertex_index(cell[0]), CGAL::SM_Vertex_index(cell[1]), CGAL::SM_Vertex_index(cell[2]));
+	}
+
+	return 0;
+}
