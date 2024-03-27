40b46892e5e74190b07183895f7eb13d3f08c675
0x11111111 <dypro_mike@outlook.com>
2024-03-27 11:21:07 +0800

    Updated comments. The bite's depth image needs to be reversed.

--- src/algorithm/IniIO.cpp
+++ src/algorithm/IniIO.cpp
@@ -1,32 +1,33 @@
 #include "stdafx.h"
 #include "IniIO.h"
 
+
 bool write_ini_file(const std::string& ini_file_name, const std::string& last_path)
 {
-    std::ofstream iniFile(ini_file_name);
-    if (!iniFile.is_open()) 
+    std::ofstream ini_file(ini_file_name);
+    if (!ini_file.is_open()) 
     {
         std::cerr << "Unable to open file for writing: " << ini_file_name << std::endl;
         return false;
     }
 
-    iniFile << "last_path=" << last_path << std::endl;
+    ini_file << "last_path=" << last_path << std::endl;
 
-    iniFile.close();
+    ini_file.close();
     return true;
 }
 
 bool read_ini_file(const std::string& ini_file_name, std::string& last_path)
 {
-    std::ifstream iniFile(ini_file_name);
-    if (!iniFile.is_open())
+    std::ifstream ini_file(ini_file_name);
+    if (!ini_file.is_open())
     {
         std::cerr << "Unable to open file for reading: " << ini_file_name << std::endl;
         return false;
     }
 
     std::string line;
-    while (std::getline(iniFile, line)) 
+    while (std::getline(ini_file, line)) 
     {
         size_t pos = line.find("last_path=");
         if (pos != std::string::npos) {
@@ -35,6 +36,6 @@ bool read_ini_file(const std::string& ini_file_name, std::string& last_path)
         }
     }
 
-    iniFile.close();
+    ini_file.close();
     return !last_path.empty();
 }
--- src/algorithm/IniIO.h
+++ src/algorithm/IniIO.h
@@ -3,5 +3,8 @@
 #include <fstream>
 #include <string>
 
+/**
+ * Interacts with an INI file to read and write the last path selected by the user.
+ */
 bool write_ini_file(const std::string& iniFileName, const std::string& lastPath);
 bool read_ini_file(const std::string& iniFileName, std::string& lastPath);
