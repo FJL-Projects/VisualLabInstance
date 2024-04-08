7125dd1282e334cbfd71daa8367589bda86cc114
0x11111111 <dypro_mike@outlook.com>
2024-04-08 17:24:16 +0800

    Added a new overload to support std::array.

--- src/algorithm/Rotation.h
+++ src/algorithm/Rotation.h
@@ -168,4 +168,68 @@ inline void AngleAxisRotatePoint(const T angle_axis[3], const T pt[3], T result[
         result[2] = pt[2] + w_cross_pt[2];
     }
 }
+
+template<typename T>
+inline void AngleAxisRotatePoint(const T angle_axis, const T pt, T result)
+{
+    const T theta2 = DotProduct(angle_axis, angle_axis);
+    if (theta2 > T(std::numeric_limits<double>::epsilon()))
+    {
+        // 非零时,使用罗德里格斯公式
+        //
+        //   result = pt costheta +
+        //            (w x pt) * sintheta +
+        //            w (w . pt) (1 - costheta)
+        //
+        //如果 angle_axis 向量的范数大于零,我们要只计算平方根,否则会被零除.
+        //
+        const T theta = sqrt(theta2);
+        const T costheta = cos(theta);
+        const T sintheta = sin(theta);
+        const T theta_inverse = 1.0 / theta;
+
+        const T w[3] = { angle_axis[0] * theta_inverse,
+                        angle_axis[1] * theta_inverse,
+                        angle_axis[2] * theta_inverse };
+        T w_cross_pt[3];
+        CrossProduct(w, pt, w_cross_pt);
+
+        const T tmp = DotProduct(w, pt) * (T(1.0) - costheta);
+        //    (w[0] * pt[0] + w[1] * pt[1] + w[2] * pt[2]) * (T(1.0) - costheta);
+
+        result[0] = pt[0] * costheta + w_cross_pt[0] * sintheta + w[0] * tmp;
+        result[1] = pt[1] * costheta + w_cross_pt[1] * sintheta + w[1] * tmp;
+        result[2] = pt[2] * costheta + w_cross_pt[2] * sintheta + w[2] * tmp;
+    }
+    else
+    {
+        // Near zero, the first order Taylor approximation of the rotation
+        // matrix R corresponding to a vector w and angle w is
+        //
+        //   R = I + hat(w) * sin(theta)
+        //
+        // But sintheta ~ theta and theta * w = angle_axis, which gives us
+        //
+        //  R = I + hat(w)
+        //
+        // and actually performing multiplication with the point pt, gives us
+        // R * pt = pt + w x pt.
+        //
+        // Switching to the Taylor expansion near zero provides meaningful
+        // derivatives when evaluated using Jets.
+        //
+        // Explicitly inlined evaluation of the cross product for
+        // performance reasons.
+        /*const T w_cross_pt[3] = { angle_axis[1] * pt[2] - angle_axis[2] * pt[1],
+                                  angle_axis[2] * pt[0] - angle_axis[0] * pt[2],
+                                  angle_axis[0] * pt[1] - angle_axis[1] * pt[0] };*/
+        T w_cross_pt[3];
+        CrossProduct(angle_axis, pt, w_cross_pt);
+
+        result[0] = pt[0] + w_cross_pt[0];
+        result[1] = pt[1] + w_cross_pt[1];
+        result[2] = pt[2] + w_cross_pt[2];
+    }
+}
+
 #endif
