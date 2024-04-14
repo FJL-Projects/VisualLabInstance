2e8d5628983d3fb4675e18094d6b953a7ccdae02
0x11111111 <dypro_mike@outlook.com>
2024-04-14 22:14:26 +0800

    Finished tangent direction calculation. Next is to read and write through INI.

--- src/main program/main.cpp
+++ src/main program/main.cpp
@@ -93,7 +93,7 @@ int main()
 		vtkIdType id = 0;
 		for (auto& mesh_pd : meshes_pd)
 		{
-			RenderPolydata(mesh_pd, pipeline->Renderer, r, g, b, opacity);
+			//RenderPolydata(mesh_pd, pipeline->Renderer, r, g, b, opacity);
 			double* bound = mesh_pd->GetBounds();
 			double center[3] = {
 				(bound[0] + bound[1]) / 2,
@@ -106,54 +106,58 @@ int main()
 		g += 0.25;
 		b += 0.25;
 
-		for (vtkIdType i = 0; i < points->GetNumberOfPoints(); ++i)
-		{
-			double p[3];
-			points->GetPoint(i, p);
-			RenderSphere(p, 0.5, pipeline->Renderer, 1, 0, 0, 1);
-		}
+		//for (vtkIdType i = 0; i < points->GetNumberOfPoints(); ++i)
+		//{
+		//	double p[3];
+		//	points->GetPoint(i, p);
+		//	RenderSphere(p, 0.5, pipeline->Renderer, 1, 0, 0, 1);
+		//}
 	}
 
 	vtkIdType spline_id = 0;
 	for (auto& meshes_pd : meshes_pd_vec)
 	{
 		vtkSmartPointer<vtkPoints> teeth_center = teeth_center_vec[spline_id++];
+		vtkSmartPointer<vtkPoints> spline_teeth_center_nearest_points = vtkSmartPointer<vtkPoints>::New();
+		vtkSmartPointer<vtkIntArray> teeth_center_id = vtkSmartPointer<vtkIntArray>::New();
+		std::vector<vtkIdType> teeth_center_id_in_spline;
+		teeth_center_id->SetName("teeth_center");
 
 		std::vector<std::vector<double>> all_spline_points;
 		vtkNew<vtkNamedColors> color;
 
 		// Create splines for x, y, and z coordinates
-		vtkNew<vtkKochanekSpline> xSpline;
-		vtkNew<vtkKochanekSpline> ySpline;
-		vtkNew<vtkKochanekSpline> zSpline;
+		vtkNew<vtkKochanekSpline> x_spline;
+		vtkNew<vtkKochanekSpline> y_spline;
+		vtkNew<vtkKochanekSpline> z_spline;
 
 		vtkNew<vtkParametricSpline> spline;
-		spline->SetXSpline(xSpline);
-		spline->SetYSpline(ySpline);
-		spline->SetZSpline(zSpline);
+		spline->SetXSpline(x_spline);
+		spline->SetYSpline(y_spline);
+		spline->SetZSpline(z_spline);
 		spline->SetPoints(teeth_center);
 
-		vtkNew<vtkParametricFunctionSource> functionSource;
-		functionSource->SetParametricFunction(spline);
-		functionSource->SetUResolution(200);
-		functionSource->SetVResolution(200);
-		functionSource->SetWResolution(200);
-		functionSource->Update();
+		vtkNew<vtkParametricFunctionSource> function_source;
+		function_source->SetParametricFunction(spline);
+		function_source->SetUResolution(200);
+		function_source->SetVResolution(200);
+		function_source->SetWResolution(200);
+		function_source->Update();
 
-		auto splinepolydata = functionSource->GetOutput();
-		vtkIdList* ptId = vtkIdList::New();
+		auto spline_polydata = function_source->GetOutput();
+		vtkIdList* pt_id = vtkIdList::New();
 
 		// Get the points from the spline polydata
-		for (vtkIdType i = 0; i < splinepolydata->GetPoints()->GetNumberOfPoints(); i++)
+		for (vtkIdType i = 0; i < spline_polydata->GetPoints()->GetNumberOfPoints(); i++)
 		{
-			ptId->InsertNextId(i);
+			pt_id->InsertNextId(i);
 		}
 
 		vtkPoints* fp = vtkPoints::New();
-		splinepolydata->GetPoints()->GetPoints(ptId, fp);
+		spline_polydata->GetPoints()->GetPoints(pt_id, fp);
 
 		// Store the spline points in a vector
-		for (vtkIdType i = 0; i < splinepolydata->GetPoints()->GetNumberOfPoints(); i++)
+		for (vtkIdType i = 0; i < spline_polydata->GetPoints()->GetNumberOfPoints(); i++)
 		{
 			std::vector<double> tmp;
 			tmp.push_back(fp->GetPoint(i)[0]);
@@ -162,7 +166,86 @@ int main()
 			all_spline_points.push_back(tmp);
 		}
 
-		// Create spheres for each spline point and add them as actors
+		
+		for (vtkIdType teeth_center_id = 0; teeth_center_id < teeth_center->GetNumberOfPoints(); ++teeth_center_id)
+		{
+			double min_distance = std::numeric_limits<double>::max();
+			vtkIdType min_distance_id = 0;
+			for (vtkIdType i = 0; i < spline_polydata->GetPoints()->GetNumberOfPoints(); i++)
+			{
+				double p[3];
+				fp->GetPoint(i, p);
+				double q[3];
+				teeth_center->GetPoint(teeth_center_id, q);
+				double distance = (p[0] - q[0]) * (p[0] - q[0]) + (p[1] - q[1]) * (p[1] - q[1]) + (p[2] - q[2]) * (p[2] - q[2]);
+				if (distance < min_distance)
+				{
+					min_distance = distance;
+					min_distance_id = i;
+				}
+				if (distance > min_distance)
+				{
+					break;
+				}
+			}
+			teeth_center_id_in_spline.push_back(min_distance_id);
+		}
+		for (auto it = teeth_center_id_in_spline.begin(); it != teeth_center_id_in_spline.end(); ++it)
+		{	
+			auto id = *it;
+			spline_teeth_center_nearest_points->InsertNextPoint(all_spline_points[id][0], all_spline_points[id][1], all_spline_points[id][2]);
+			std::cout << " " << id << " ";
+			if (it == teeth_center_id_in_spline.end() - 1)  // Reached the last element
+			{
+				vtkIdType prev_id = id - 1;
+				Point_3 prev_point(all_spline_points[prev_id][0], all_spline_points[prev_id][1], all_spline_points[prev_id][2]);
+				Point_3 curr_point(all_spline_points[id][0], all_spline_points[id][1], all_spline_points[id][2]);
+				Vector_3 direction(prev_point, curr_point);
+				direction = direction / std::sqrt(direction.squared_length());
+				std::cout << direction << std::endl;
+
+				Point_3 source_point(prev_point);
+				Point_3 target_point(prev_point + direction * 10);
+				RenderLine(source_point, target_point, pipeline->Renderer, 0, 1, 0, 1);
+			}
+			else
+			{
+				vtkIdType next_id = id + 1;
+				Point_3 curr_point(all_spline_points[id][0], all_spline_points[id][1], all_spline_points[id][2]);
+				Point_3 next_point(all_spline_points[next_id][0], all_spline_points[next_id][1], all_spline_points[next_id][2]);
+				Vector_3 direction(curr_point, next_point);
+				direction = direction / std::sqrt(direction.squared_length());
+				std::cout << direction << std::endl;
+
+				Point_3 source_point(curr_point);
+				Point_3 target_point(curr_point + direction * 10);
+				RenderLine(source_point, target_point, pipeline->Renderer, 0, 1, 0, 1);
+			}
+		}
+		std::cout << std::endl;
+		
+		// Glyph the points
+		vtkNew<vtkSphereSource> points_actor;
+		points_actor->SetPhiResolution(21);
+		points_actor->SetThetaResolution(21);
+		points_actor->SetRadius(0.3);
+
+		// Create a polydata to store the points
+		vtkNew<vtkPolyData> point_polydata;
+		point_polydata->SetPoints(spline_teeth_center_nearest_points);
+
+		vtkNew<vtkGlyph3DMapper> point_mapper;
+		point_mapper->SetInputData(point_polydata);
+		point_mapper->SetSourceConnection(points_actor->GetOutputPort());
+
+		 //Create an actor for the points
+		vtkNew<vtkActor> point_actor;
+		point_actor->SetMapper(point_mapper);
+		point_actor->GetProperty()->SetColor(color->GetColor3d("Peacock").GetData());
+		point_actor->GetProperty()->SetOpacity(1);
+		pipeline->Renderer->AddActor(point_actor);
+
+			// Create spheres for each spline point and add them as actors
 		for (int i = 0; i < all_spline_points.size(); i++)
 		{
 			vtkNew<vtkSphereSource> vtk_sphere;
@@ -181,38 +264,38 @@ int main()
 		}
 
 		// Create an actor for the spline line
-		vtkNew<vtkPolyDataMapper> LineMapper;
-		LineMapper->SetInputConnection(functionSource->GetOutputPort());
+		vtkNew<vtkPolyDataMapper> line_mapper;
+		line_mapper->SetInputConnection(function_source->GetOutputPort());
 
-		vtkNew<vtkActor> LineActor;
-		LineActor->SetMapper(LineMapper);
-		LineActor->GetProperty()->SetColor(color->GetColor3d("Red").GetData());
-		LineActor->GetProperty()->SetLineWidth(1.0);
+		vtkNew<vtkActor> line_actor;
+		line_actor->SetMapper(line_mapper);
+		line_actor->GetProperty()->SetColor(color->GetColor3d("Red").GetData());
+		line_actor->GetProperty()->SetLineWidth(1.0);
 
-		// Glyph the points
-		vtkNew<vtkSphereSource> PointsSphere;
-		PointsSphere->SetPhiResolution(21);
-		PointsSphere->SetThetaResolution(21);
-		PointsSphere->SetRadius(1);
+		//// Glyph the points
+		//vtkNew<vtkSphereSource> points_actor;
+		//points_actor->SetPhiResolution(21);
+		//points_actor->SetThetaResolution(21);
+		//points_actor->SetRadius(1);
 
-		// Create a polydata to store the points
-		vtkNew<vtkPolyData> PointPolyData;
-		PointPolyData->SetPoints(teeth_center);
+		//// Create a polydata to store the points
+		//vtkNew<vtkPolyData> point_polydata;
+		//point_polydata->SetPoints(teeth_center);
 
-		vtkNew<vtkGlyph3DMapper> pointMapper;
-		pointMapper->SetInputData(PointPolyData);
-		pointMapper->SetSourceConnection(PointsSphere->GetOutputPort());
+		//vtkNew<vtkGlyph3DMapper> point_mapper;
+		//point_mapper->SetInputData(point_polydata);
+		//point_mapper->SetSourceConnection(points_actor->GetOutputPort());
 
-		// Create an actor for the points
-		vtkNew<vtkActor> PointActor;
-		PointActor->SetMapper(pointMapper);
-		PointActor->GetProperty()->SetColor(color->GetColor3d("Peacock").GetData());
-		PointActor->GetProperty()->SetOpacity(1);
+		// //Create an actor for the points
+		//vtkNew<vtkActor> point_actor;
+		//point_actor->SetMapper(point_mapper);
+		//point_actor->GetProperty()->SetColor(color->GetColor3d("Peacock").GetData());
+		//point_actor->GetProperty()->SetOpacity(1);
 
-		//m_spline_actor_vec.push_back(PointActor);
-		//m_spline_actor_vec.push_back(LineActor);
-		pipeline->Renderer->AddActor(PointActor);
-		pipeline->Renderer->AddActor(LineActor);
+		//m_spline_actor_vec.push_back(point_actor);
+		//m_spline_actor_vec.push_back(line_actor);
+		//pipeline->Renderer->AddActor(point_actor);
+		pipeline->Renderer->AddActor(line_actor);
 	}
 	
 
