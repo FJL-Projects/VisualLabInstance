5b0be4dcc5fa86d51e430eee7b51936b3681c56e
0x11111111 <dypro_mike@outlook.com>
2024-04-28 17:47:17 +0800

    Built a cylinder between two holes and smoothed the bridge.

--- src/main program/main.cpp
+++ src/main program/main.cpp
@@ -4,38 +4,17 @@
 #include "simpleRender.h"
 #include "IOManip.hpp"
 #include <MRMesh/MRMeshLoad.h>
+#include <MRMesh/MRMeshSave.h>
 #include <MRMesh/MRId.h>
 #include <MRMesh/MRMesh.h>
 #include <MRMesh/MRBitSetParallelFor.h>
 #include <MRMesh/MRMeshTopology.h>
 #include <MRMesh/MRExpected.h>
+#include <MRMesh/MRMeshFillHole.h>
+#include <MRMesh/MRpositionVertsSmoothly.h>
+#include <MRMesh/MRMeshDecimate.h>
 
 vtkRenderPipeline* pipeline;
-
-/**
- * @brief Generate a 4-digit number string with leading zeros.
- *
- * This function takes an integer and converts it into a string representation
- * with a fixed width of 4 characters. If the number has less than 4 digits,
- * leading zeros are added to pad the string to the desired width.
- *
- * @param number The input integer to be converted.
- * @return A string representation of the input number with leading zeros.
- *
- * @note The function uses std::ostringstream, std::setw(), and std::setfill()
- *       to format the output string.
- *
- * @example
- *   int num = 42;
- *   std::string num_str = generate_leading_zero_number_str(num);
- *   // num_str will be "0042"
- */
-std::string generate_leading_zero_number_str(int number)
-{
-	std::ostringstream stream;
-	stream << std::setw(4) << std::setfill('0') << number;
-	return stream.str();
-}
 
 void RightPress(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData)
 {
@@ -66,23 +45,69 @@ int main()
 	using namespace MR;
 	pipeline = new vtkRenderPipeline();
 
-	MR::Mesh mrmesh = *MR::MeshLoad::fromAnySupportedFormat("data/11.stl");
-	std::vector<vertex_descriptor> vertices_list(mrmesh.topology.vertSize());
-	
-	SurfaceMesh sm = MRMeshToSurfaceMesh(mrmesh);
-	Mesh converted = SurfaceMeshToMRMesh(sm);
-	
-	MeshSave::toAnySupportedFormat(converted, "data/converted.stl");
+	Mesh mesh = *MR::MeshLoad::fromAnySupportedFormat("data/stitch_holes.stl");
 
-	RenderPolydata(MRMeshToPolyData(mrmesh), pipeline->Renderer);
+	Mesh old_mesh(mesh);
+	std::vector<EdgeId> edges = mesh.topology.findHoleRepresentiveEdges();
 
-	vtkNew<vtkSTLReader> reader;
-	reader->SetFileName("data/converted.stl");
-	reader->Update();
-	vtkSmartPointer<vtkPolyData> polydata = reader->GetOutput();
+	if (edges.size() != 2)
+	{
+		std::cerr << "Two holes is required in the mesh." << std::endl;
+		return 1;
+	}
 
-	Mesh converted_from_pd = PolyDataToMRMesh(polydata);
-	MeshSave::toAnySupportedFormat(converted_from_pd, "data/converted_from_pd.ply");
+	buildCylinderBetweenTwoHoles(mesh);
+
+	MeshSave::toAnySupportedFormat(mesh, "data/not_remeshed_stitched_holes.stl");
+
+	FaceBitSet cylinder_faces(mesh.topology.faceSize());
+
+	size_t old_mesh_face_size = old_mesh.topology.faceSize();
+	std::cout << "Old mesh face size: " << old_mesh_face_size << std::endl;
+	std::cout << "New mesh face size: " << mesh.topology.faceSize() << std::endl;
+
+	// Outer params is old_mesh_face_size and ref cylinder_faces;
+	// Each element in getValidFaces() is passed to the lambda function's FaceId f
+	BitSetParallelFor(mesh.topology.getValidFaces(), [old_mesh_face_size, &cylinder_faces](FaceId f)
+		{
+			if (f.get() > old_mesh_face_size)
+			{
+				cylinder_faces.set(f);
+			}
+		});
+
+	//for (auto f : mesh.topology.getValidFaces())
+	//{
+	//	if (f.get() > old_mesh_face_size)
+	//	{
+	//		cylinder_faces.set(f);
+	//	}
+	//}
+
+	RemeshSettings remesh_settings;
+	remesh_settings.targetEdgeLen = 0.5f;
+	remesh_settings.region = &cylinder_faces;
+	remesh(mesh, remesh_settings);
+
+	MeshSave::toAnySupportedFormat(mesh, "data/stitched_holes.stl");
+	size_t old_mesh_vert_size = old_mesh.topology.vertSize();
+	std::cout << "Old mesh vert size: " << old_mesh_vert_size << std::endl;
+    std::cout << "New mesh vert size: " << mesh.topology.vertSize() << std::endl;
+	VertBitSet cylinder_points(mesh.topology.vertSize());
+	BitSetParallelFor(mesh.topology.getValidVerts(), [old_mesh_vert_size, &cylinder_points](VertId v)
+		{
+			if (v.get() > old_mesh_vert_size)
+			{
+				cylinder_points.set(v);
+			}
+		});
+
+	MR::positionVertsSmoothly(mesh, cylinder_points);
+	MeshSave::toAnySupportedFormat(mesh, "data/smoothed_stitched_holes.stl");
+
+	// MRMeshToPolyData may cause crashes. Still need to investigate why.
+	vtkSmartPointer<vtkPolyData> polydata = CGAL_Surface_Mesh2VTK_PolyData(MRMeshToSurfaceMesh(mesh));
+	RenderPolydata(polydata, pipeline->Renderer);
 
 	// Set up the camera and interactor.
 	pipeline->Renderer->GetActiveCamera()->SetParallelProjection(1);
